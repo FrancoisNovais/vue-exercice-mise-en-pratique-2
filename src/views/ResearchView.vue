ed864fd412ab4d462bb5e319c359fb195e5b3d2a
francois6381 <francois.novais@gmail.com>
2024-09-28 00:23:00 +0200

    Add data.js and ResearchView template

--- src/views/ResearchView.vue
+++ src/views/ResearchView.vue
@@ -1,7 +1,18 @@
-<script setup></script>
+<script setup>
+import { personsList } from '@/assets/data'
+</script>
 
 <template>
   <main>
-    <p>Research Page</p>
+    <div class="left-column">
+      <h1>Research by name</h1>
+      <form v-on:submit="handleSubmit">
+        <input type="text" name="name" id="name" />
+        <button>Search</button>
+      </form>
+    </div>
+    <div class="right-column">
+      <p>This name doesn't exist in the list, sorry !!</p>
+    </div>
   </main>
 </template>
