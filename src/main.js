5a6e1e319a9177fd3bf05635a0609f9f61660300
francois6381 <francois.novais@gmail.com>
2024-10-01 13:48:00 +0200

    ProfileView function

--- src/main.js
+++ src/main.js
@@ -1,11 +1,21 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
+import { createApp, ref } from 'vue'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
 app.use(router)
+
+const userInfos = ref({
+  gender: 'female',
+  firstname: 'Sophia',
+  lastname: 'Rodriguez',
+  age: 31,
+  job: 'environmental scientist'
+})
+
+app.provide('GlobalStore', { userInfos: userInfos })
 
 app.mount('#app')
--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,7 +1,22 @@
-<script setup></script>
+<script setup>
+import { RouterLink } from 'vue-router'
+
+import { inject } from 'vue'
+
+const Store = inject('GlobalStore')
+</script>
 
 <template>
   <main>
-    <p>Profile Page</p>
+    <h1>Your profile</h1>
+
+    <div>
+      <p>Firstname : {{ Store.userInfos.value.firstname }}</p>
+      <p>Lastname : {{ Store.userInfos.value.lastname }}</p>
+      <p>Age : {{ Store.userInfos.value.age }}</p>
+      <p>Job : {{ Store.userInfos.value.job }}</p>
+    </div>
+
+    <button><RouterLink :to="{ name: 'edit' }"> Edit profile </RouterLink></button>
   </main>
 </template>
