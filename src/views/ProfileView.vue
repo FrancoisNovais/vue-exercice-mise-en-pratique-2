45f5ffeeb37a1c0020e6726c39a6e23a20f47773
francois6381 <francois.novais@gmail.com>
2024-10-03 20:53:40 +0200

    ProfileView CSS

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -8,15 +8,35 @@ const Store = inject('GlobalStore')
 
 <template>
   <main>
-    <h1>Your profile</h1>
+    <div class="container">
+      <h1>Your profile</h1>
 
-    <div>
-      <p>Firstname : {{ Store.userInfos.value.firstname }}</p>
-      <p>Lastname : {{ Store.userInfos.value.lastname }}</p>
-      <p>Age : {{ Store.userInfos.value.age }}</p>
-      <p>Job : {{ Store.userInfos.value.job }}</p>
+      <div>
+        <p>Firstname : {{ Store.userInfos.value.firstname }}</p>
+        <p>Lastname : {{ Store.userInfos.value.lastname }}</p>
+        <p>Age : {{ Store.userInfos.value.age }}</p>
+        <p>Job : {{ Store.userInfos.value.job }}</p>
+      </div>
+
+      <RouterLink :to="{ name: 'edit' }"> <p>Edit profile</p> </RouterLink>
     </div>
-
-    <button><RouterLink :to="{ name: 'edit' }"> Edit profile </RouterLink></button>
   </main>
 </template>
+
+<style scoped>
+.container {
+  margin-top: 20px;
+}
+a {
+  text-decoration: none;
+}
+a p {
+  margin-top: 20px;
+  color: #f99600;
+  border: 2px solid #f99600;
+  padding: 7px;
+  width: 100px;
+  text-align: center;
+  border-radius: 10px;
+}
+</style>
