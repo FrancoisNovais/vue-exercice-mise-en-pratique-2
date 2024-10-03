37b342a28b914508c2230c2ed3e3211a21256a7d
francois6381 <francois.novais@gmail.com>
2024-10-03 17:41:57 +0200

    ResearchView CSS

--- src/views/ResearchView.vue
+++ src/views/ResearchView.vue
@@ -17,25 +17,63 @@ const handleSubmit = () => {
 
 <template>
   <main>
-    <div class="left-column">
-      <h1>Research by name</h1>
-      <form v-on:submit.prevent="handleSubmit">
-        <input type="text" name="name" id="name" placeholder="Name" v-model="firstname" />
-        <button>Search</button>
-      </form>
-    </div>
-    <div class="right-column">
-      <h3 v-if="result === null">Enter a firstname</h3>
+    <div class="container">
+      <div class="left-column">
+        <h1>Research by name</h1>
+        <form v-on:submit.prevent="handleSubmit">
+          <input type="text" name="name" id="name" placeholder="Name" v-model="firstname" />
+          <button>Search</button>
+        </form>
+      </div>
+      <div class="right-column">
+        <h3 v-if="result === null">Enter a firstname</h3>
 
-      <h3 v-else-if="result === undefined">This name doesn't exist in the list, sorry !!</h3>
+        <h3 v-else-if="result === undefined">This name doesn't exist in the list, sorry !!</h3>
 
-      <div v-else>
-        <h2>Found it !!!</h2>
-        <p>{{ result.firstname }} {{ result.lastname }}</p>
-        <p>{{ result.age }} ans</p>
-        <p>{{ result.job }}</p>
+        <div v-else class="userFound">
+          <h2>Found it !!!</h2>
+          <div>
+            <p>{{ result.firstname }} {{ result.lastname }}</p>
+            <p>{{ result.age }} ans</p>
+            <p>{{ result.job }}</p>
+          </div>
+        </div>
       </div>
     </div>
   </main>
 </template>
-<style scoped></style>
+<style scoped>
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+}
+.left-column {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+button {
+  margin-left: 20px;
+}
+h2 {
+  font-size: 26px;
+  font-weight: bold;
+  margin-bottom: 20px;
+}
+h3 {
+  font-size: 20px;
+  font-weight: bold;
+}
+
+.userFound {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+.userFound > div {
+  border: 1px solid black;
+  border-radius: 10px;
+  padding: 10px 25px;
+}
+</style>
